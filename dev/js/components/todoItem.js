d204a31eb1ec134b79be26752a1277efd4680885
Adolfo Gutierrez <adolfo.gutierrez.87@gmail.com>
2017-07-06 10:44:03 -0600

    Reduce size bundle

--- dev/js/components/todoItem.js
+++ dev/js/components/todoItem.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import moment from 'moment'
+import moment from 'moment';
 
 class TodoItem extends React.Component {
   render() {
     return(
-        <a className="panel-block">
-            <b>{this.props.item.title}</b> - <small className="is-left"> Due: {moment(this.props.item.dueDate).format('dddd, MMMM Do, YYYY')}</small>
-        </a>
+      <a className="panel-block">
+        <b>{this.props.item.title}</b> - <small className="is-left"> Due: {moment(this.props.item.dueDate).format('dddd, MMMM Do, YYYY')}</small>
+      </a>
     );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const uglifyPlugin = require('uglifyjs-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
+	devtool: 'nosources-source-map',
 	entry: ['./dev/js/main.js',  './dev/sass/master.scss'],
 	watch: true,
 	output: {
@@ -11,8 +12,7 @@ module.exports = {
     	path: path.resolve(`${__dirname}/build`, 'js')
 	},
 	resolveLoader: {
-		modules: ["node_modules"],
-		moduleExtensions: ['-loader']
+		modules: ["node_modules"]
 	},
 	module: {
 		rules: [
@@ -27,7 +27,7 @@ module.exports = {
 				exclude: /(node_modules|bower_components)/,
 				use: [
 					{
-						loader: 'babel',
+						loader: 'babel-loader',
 						options: {
 							presets: ['es2015','react'],
 							comments: false
@@ -37,18 +37,42 @@ module.exports = {
 			},
 			// {
 			// 	test: /\.css$/,
+			// 	exclude: /(node_modules|bower_components)/,
 			// 	use: ExtractTextPlugin.extract({
 			// 		use: 'css-loader?importLoaders=1&minimize=true',
 			// 	}),
 			// },
 			{
 				test: /\.(sass|scss)$/,
+				exclude: /(node_modules|bower_components)/,
 				use: ExtractTextPlugin.extract(['css-loader?minimize=true', 'sass-loader'])
 			}
 		]
 	},
 	plugins: [
-		new uglifyPlugin({output: {comments: false}}),
+		new uglifyPlugin({
+			mangle: true,
+			compress: {
+				warnings: false, // Suppress uglification warnings
+				pure_getters: true,
+				unsafe: true,
+				unsafe_comps: true,
+				screw_ie8: true
+			},
+			sourceMap: false,
+			output: {
+				comments: false
+			},
+			compressor: {
+				warnings: false
+			}
+		}),
+		new webpack.DefinePlugin({
+      		'process.env': {
+				'NODE_ENV': JSON.stringify('production')
+			}
+		}),
+		new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
 		new ExtractTextPlugin({
 			filename: '../css/master.css',
 			allChunks: false,
